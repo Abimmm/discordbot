6758641f81de2272c89852465d2f2a7f4d01a07e
Abimmm <58972631+Abimmm@users.noreply.github.com>
2020-09-27 19:05:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var bot = new Discord.Client();
 
 bot.on("ready", function(){
     console.log('Bot telah aktif');
-bot.user.setActivity('@putribali.id', { type : 'WATCHING'}).catch(console.error);
 	
 })
 
-bot.login("NzUzNTU3NjIwNjE0MzY1MjU2.X1n7OQ.gBcgR_RkEhZr7i6tQ939cIuCQGw");
+bot.login("#TOKENBOTMU");
